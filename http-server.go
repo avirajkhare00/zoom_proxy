cfa96b926587125b8bf6c9d574fbe285a06f802e
Aviraj Khare <avirajkhare00@gmail.com>
2020-04-19 03:32:16 +0530

    [Aviraj] Add /zoom endpoint for redirect

--- http-server.go
+++ http-server.go
@@ -6,9 +6,16 @@ import (
 )
 
 func main() {
+
+	zoomPersonalMeetingURL := "https://gojek.zoom.us/my/avirajkhare00"
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte("You are reading a simple text.\n"))
+	})
+
+	http.HandleFunc("/zoom", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, zoomPersonalMeetingURL, 302)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
